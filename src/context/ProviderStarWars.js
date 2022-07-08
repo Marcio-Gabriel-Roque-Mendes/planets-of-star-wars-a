3b31f3365054eb1063e99ffcbb48011f5ce4f59e
Marcio Gabriel <marcio_2411@hotmail.com>
2022-07-08 15:14:23 -0300

    Iniciando o Projeto Star Wars Planets Search

---
+++ src/context/ProviderStarWars.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import ContextStarWars from './ContextStarWars';
+
+const Provider = ({ children }) => {
+  const [name, setName] = useState('');
+  const [rotationPeriod, setRotationPeriod] = useState(0);
+  const [orbitalPeriod, setOrbitalPeriod] = useState(0);
+  const [diameter, setDiameter] = useState(0);
+  const [climate, setClimate] = useState('');
+  const [gravity, setGravity] = useState('');
+  const [] = useState();
+  const [] = useState();
+  const [] = useState();
+  const [] = useState();
+  const [] = useState();
+  const [] = useState();
+  const [] = useState();
+};
