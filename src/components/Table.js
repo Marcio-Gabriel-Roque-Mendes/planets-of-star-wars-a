6f3d583743fa8dd5b4360ab1c3402b3110a4c68e
Marcio Gabriel <marcio_2411@hotmail.com>
2022-07-19 20:10:10 -0300

    requisitos 2, 3 e 4 concluidos

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,78 +1,146 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContextStarWars from '../context/ContextStarWars';
 // import PropTypes from 'prop-types';
 
 function Table() {
-  const valorDoContexto = useContext(ContextStarWars);
-  console.log(valorDoContexto.arrayPlanetas.map((planet) => planet), 'valores');
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Rotation Period
-          </th>
-          <th>
-            Orbital Period
-          </th>
-          <th>
-            Diameter
-          </th>
-          <th>
-            Climate
-          </th>
-          <th>
-            Gravity
-          </th>
-          <th>
-            Terrain
-          </th>
-          <th>
-            Surface Water
-          </th>
-          <th>
-            Population
-          </th>
-          <th>
-            Films
-          </th>
-          <th>
-            Created
-          </th>
-          <th>
-            Edited
-          </th>
-          <th>
-            URL
-          </th>
-        </tr>
-      </thead>
+  const [coluna, setColuna] = useState('population');
+  const [operador, setOperador] = useState('maior que');
+  const [valor, setValor] = useState(0);
 
-      <tbody>
-        {
-          valorDoContexto.arrayPlanetas.map((planeta) => (
-            <tr key={ planeta.name }>
-              <td>{ planeta.name}</td>
-              <td>{ planeta.rotation_period}</td>
-              <td>{ planeta.orbital_period}</td>
-              <td>{ planeta.diameter}</td>
-              <td>{ planeta.climate}</td>
-              <td>{ planeta.gravity}</td>
-              <td>{ planeta.terrain}</td>
-              <td>{ planeta.surface_water}</td>
-              <td>{ planeta.population}</td>
-              <td>{ planeta.films}</td>
-              <td>{ planeta.created}</td>
-              <td>{ planeta.edited}</td>
-              <td>{ planeta.url}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+  const valorDoContexto = useContext(ContextStarWars);
+  const { setInputText, inputText, setFiltros } = valorDoContexto;
+  // console.log(valorDoContexto.arrayPlanetas.map((planet) => planet), 'valores');
+
+  return (
+    <div>
+      <input
+        data-testid="name-filter"
+        placeholder="Digite Aqui"
+        value={ inputText }
+        onChange={ (event) => setInputText(event.target.value) }
+      />
+      <hr />
+
+      <label htmlFor="column-filter">
+        Coluna:
+        <select
+          data-testid="column-filter"
+          value={ coluna }
+          onChange={ (event) => setColuna(event.target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+          <option value="diameter">diameter</option>
+
+        </select>
+      </label>
+
+      <label htmlFor="comparison-filter">
+        Operador:
+        <select
+          data-testid="comparison-filter"
+          value={ operador }
+          onChange={ (event) => setOperador(event.target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+
+      <label htmlFor="value-filter">
+        Valor:
+        <input
+          type="number"
+          data-testid="value-filter"
+          value={ valor }
+          onChange={ (event) => (setValor(event.target.value)) }
+          // onChange={ (event) => (event.target.value === ''.trim() ? 0 : (setValor(parseFloat(event.target.value)))) }
+        />
+      </label>
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => (
+          setFiltros((prevState) => ([...prevState, { coluna, operador, valor }]))) }
+      >
+        Filtrar
+      </button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Rotation Period
+            </th>
+            <th>
+              Orbital Period
+            </th>
+            <th>
+              Diameter
+            </th>
+            <th>
+              Climate
+            </th>
+            <th>
+              Gravity
+            </th>
+            <th>
+              Terrain
+            </th>
+            <th>
+              Surface Water
+            </th>
+            <th>
+              Population
+            </th>
+            <th>
+              Films
+            </th>
+            <th>
+              Created
+            </th>
+            <th>
+              Edited
+            </th>
+            <th>
+              URL
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            valorDoContexto.arrayPlanetas
+              .filter((item) => item.name.includes(valorDoContexto.inputText))
+              .map((planeta) => (
+                <tr key={ planeta.name }>
+                  <td>{planeta.name}</td>
+                  <td>{planeta.rotation_period}</td>
+                  <td>{planeta.orbital_period}</td>
+                  <td>{planeta.diameter}</td>
+                  <td>{planeta.climate}</td>
+                  <td>{planeta.gravity}</td>
+                  <td>{planeta.terrain}</td>
+                  <td>{planeta.surface_water}</td>
+                  <td>{planeta.population}</td>
+                  <td>{planeta.films}</td>
+                  <td>{planeta.created}</td>
+                  <td>{planeta.edited}</td>
+                  <td>{planeta.url}</td>
+                </tr>
+              ))
+          }
+        </tbody>
+      </table>
+    </div>
+
   );
 }
 
--- src/context/ProviderStarWars.js
+++ src/context/ProviderStarWars.js
@@ -5,10 +5,17 @@ import ContextStarWars from './ContextStarWars';
 
 const ProviderStarWars = ({ children }) => {
   const [arrayPlanetas, setArrayPlanetas] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [filtros, setFiltros] = useState([]);
+  const [data, setData] = useState([]);
 
   const contextValue = { /* Objeto contextValue */
     arrayPlanetas,
     setArrayPlanetas,
+    inputText,
+    setInputText,
+    filtros,
+    setFiltros,
   };
 
   const getStarWarsPlanets = () => { // SO CHAMADA A API E SALVAR NO ESTADO (COM DUAS FORMAS DE FAZER)
@@ -17,6 +24,8 @@ const ProviderStarWars = ({ children }) => {
       .then((response) => {
         console.log(response.results);
         setArrayPlanetas(response.results);
+
+        setData(response.results);
 
         // response.results.map((planet) => setArrayPlanetas((prevState) => [...prevState, planet.name])); //FORMA 1
 
@@ -28,6 +37,63 @@ const ProviderStarWars = ({ children }) => {
   useEffect(() => {
     getStarWarsPlanets();
   }, []);
+
+  // const numero = 3000;
+
+  // useEffect(() => {
+  //   setArrayPlanetas(data);
+  //   const comparacao = arrayPlanetas.filter((planeta) => Number(planeta
+  //     .population) > numero);
+  //   setArrayPlanetas(comparacao);
+  // }, [filtros]);
+
+  // useEffect(() => {
+  //   setArrayPlanetas(data);
+
+  //   if (filtros[0]?.operador === 'maior que') {
+  //     const comparacao = arrayPlanetas
+  //       .filter((planeta) => Number(planeta[filtros[0]
+  //         .coluna]) > Number(filtros[0].valor));
+  //     return setArrayPlanetas(comparacao);
+  //   }
+  //   if (filtros[0]?.operador === 'menor que') {
+  //     const comparacao = arrayPlanetas
+  //       .filter((planeta) => Number(planeta[filtros[0]
+  //         .coluna]) < Number(filtros[0].valor));
+  //     return setArrayPlanetas(comparacao);
+  //   }
+  //   if (filtros[0]?.operador === 'igual a') {
+  //     const comparacao = arrayPlanetas
+  //       .filter((planeta) => Number(planeta[filtros[0]
+  //         .coluna]) === Number(filtros[0].valor));
+  //     return setArrayPlanetas(comparacao);
+  //   }
+  // }, [filtros]);
+
+  useEffect(() => {
+    setArrayPlanetas(data);
+
+    filtros.forEach((filtro) => {
+      if (filtro.operador === 'maior que') {
+        const comparacao = arrayPlanetas
+          .filter((planeta) => Number(planeta[filtro
+            .coluna]) > Number(filtro.valor));
+        return setArrayPlanetas(comparacao);
+      }
+      if (filtro.operador === 'menor que') {
+        const comparacao = arrayPlanetas
+          .filter((planeta) => Number(planeta[filtro
+            .coluna]) < Number(filtro.valor));
+        return setArrayPlanetas(comparacao);
+      }
+      if (filtro.operador === 'igual a') {
+        const comparacao = arrayPlanetas
+          .filter((planeta) => Number(planeta[filtro
+            .coluna]) === Number(filtro.valor));
+        return setArrayPlanetas(comparacao);
+      }
+    });
+  }, [filtros]);
 
   return (
     <ContextStarWars.Provider value={ contextValue }>
