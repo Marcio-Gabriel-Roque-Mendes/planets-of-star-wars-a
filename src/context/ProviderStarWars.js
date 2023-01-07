66089c8a09a2a8373de49db9004aa1f510ede6f8
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-01-07 20:31:01 -0300

    Feat(API): nova api sendo consumida, pois a anterior caiu, quando o Heroku começou a cobrar

--- src/context/ProviderStarWars.js
+++ src/context/ProviderStarWars.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ContextStarWars from './ContextStarWars';
@@ -10,7 +9,6 @@ const ProviderStarWars = ({ children }) => {
   const [data, setData] = useState([]);
   const [order, setOrder] = useState();
   // const [filterByNumericValues, setFilterByNumericValues] = useState([]);
-
   const contextValue = { /* Objeto contextValue */
     arrayPlanetas,
     setArrayPlanetas,
@@ -25,36 +23,30 @@ const ProviderStarWars = ({ children }) => {
   };
 
   const getStarWarsPlanets = () => { // SO CHAMADA A API E SALVAR NO ESTADO (COM DUAS FORMAS DE FAZER)
-    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    // fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+    fetch('https://swapi.dev/api/planets/')
       .then((response) => response.json())
       .then((response) => {
         setArrayPlanetas(response.results.sort((a, b) => a.name.localeCompare(b.name)));
-
         setData(response.results.sort((a, b) => a.name.localeCompare(b.name)));
-
         // response.results.map((planet) => setArrayPlanetas((prevState) => [...prevState, planet.name])); //FORMA 1
-
         // response.results
         //   .map((planet) => setArrayPlanetas([...arrayPlanetas, planet])); // FORMA 2
       });
   };
-
   useEffect(() => {
     getStarWarsPlanets();
   }, []);
-
   // const numero = 3000;
-
   // useEffect(() => {
   //   setArrayPlanetas(data);
   //   const comparacao = arrayPlanetas.filter((planeta) => Number(planeta
   //     .population) > numero);
   //   setArrayPlanetas(comparacao);
   // }, [filtros]);
-
   // useEffect(() => {
   //   setArrayPlanetas(data);
-
   //   if (filtros[0]?.operador === 'maior que') {
   //     const comparacao = arrayPlanetas
   //       .filter((planeta) => Number(planeta[filtros[0]
@@ -74,11 +66,8 @@ const ProviderStarWars = ({ children }) => {
   //     return setArrayPlanetas(comparacao);
   //   }
   // }, [filtros]);
-
   let dados = [...data];
-
   const menosUm = -1;
-
   const ordenarASC = () => {
     if (order && order.sort === 'ASC') {
       dados.sort((a, b) => {
@@ -92,7 +81,6 @@ const ProviderStarWars = ({ children }) => {
       });
     }
   };
-
   const ordenarDESC = () => {
     if (order && order.sort === 'DESC') {
       dados.sort((a, b) => {
@@ -106,11 +94,9 @@ const ProviderStarWars = ({ children }) => {
       });
     }
   };
-
   useEffect(() => {
     // let dados = [...data]; tirado de dentro desse useEffect,
     // para poder ser usado na funcao ordenar() tambem
-
     filtros.forEach((filtro) => {
       if (filtro.operador === 'maior que') {
         dados = dados
@@ -129,20 +115,16 @@ const ProviderStarWars = ({ children }) => {
       }
     });
     setArrayPlanetas(dados);
-
     ordenarASC();
     ordenarDESC();
   }, [filtros, order]);
-
   return (
     <ContextStarWars.Provider value={ contextValue }>
       {children}
     </ContextStarWars.Provider>
   );
 };
-
 ProviderStarWars.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
 export default ProviderStarWars;
